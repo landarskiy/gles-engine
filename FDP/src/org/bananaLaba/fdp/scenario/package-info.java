c5770f13627d50b98e61f634685e2dd8fd89f378
Judzin <Judzin@10.80.61.45>
2014-04-14 15:45:05 +0300

    FDP: Some minor cleanup.

--- FDP/src/org/bananaLaba/fdp/scenario/package-info.java
+++ FDP/src/org/bananaLaba/fdp/scenario/package-info.java
@@ -2,3 +2,4 @@ package org.bananaLaba.fdp.scenario;
 // FIXME: add possibility to call stored beans.
 // TODO: add features to interact with an interface similar to the container factory's provided by an existing
 // container to be able to dynamically add/modify/delete bean definitions.
+// TODO: add a feature to call not only methods on container-managed beans, but also fields and properties.
