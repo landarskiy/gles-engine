408499704745756bedae41bb5aacc6b7fbf5a18b
Judzin <Judzin@10.80.61.45>
2014-04-19 12:58:52 +0300

    FDP & IoC: some code clean-up.

--- FDP/src/org/bananaLaba/fdp/scenario/package-info.java
+++ FDP/src/org/bananaLaba/fdp/scenario/package-info.java
@@ -1,5 +1,6 @@
 package org.bananaLaba.fdp.scenario;
-// FIXME: add possibility to call stored beans.
+// TODO: add feature of removing values from the transient store.
+// TODO: add scopes for the transient store. At least the following types: tag, global, parent.
 // TODO: add features to interact with an interface similar to the container factory's provided by an existing
 // container to be able to dynamically add/modify/delete bean definitions.
 // TODO: add a feature to call not only methods on container-managed beans, but also fields and properties.
--- IoC/src/org/bananaLaba/ioc/package-info.java
+++ IoC/src/org/bananaLaba/ioc/package-info.java
@@ -1,1 +1,2 @@
 package org.bananaLaba.ioc;
+// TODO: add possibility to remove single beans as well as bean groups from singleton cache.
