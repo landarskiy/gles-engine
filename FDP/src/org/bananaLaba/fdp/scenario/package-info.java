8cf1130f6f7a40400d70f5382ec70c8197ce33bb
Judzin <Judzin@Judzin-PC>
2014-04-24 19:05:40 +0300

    FDP: some code cleanup.

--- FDP/src/org/bananaLaba/fdp/scenario/package-info.java
+++ FDP/src/org/bananaLaba/fdp/scenario/package-info.java
@@ -9,3 +9,4 @@ package org.bananaLaba.fdp.scenario;
 // TODO: add features to interact with an interface similar to the container factory's provided by an existing
 // container to be able to dynamically add/modify/delete bean definitions.
 // TODO: add a feature to call not only methods on container-managed beans, but also fields and properties.
+// TODO: add feature of allowed tag attribute management.
